21390972c7d32b325e1660d67329ede2c467b511
marcolado <marxdo21@mail.com>
2022-10-25 16:45:22 -0300

    commit 10/22.2

---
+++ Controle/for.js
@@ -0,0 +1,13 @@
+let contador = 1
+while(contador <= 10){
+ console.log(`contador = ${contador}`)
+  contador++  
+}
+//função de laço de repetição usando while e for com o mesmo resultado
+for( i = 1; i <=10 ; i++){
+    console.log(`contador = ${i}`)
+}
+const notas = [2.3,5.6,5.5,6.6]
+for (let i =0; i < notas.length; i ++){
+    console.log(`notas = ${notas[i]}`)   
+}
